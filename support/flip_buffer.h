1c2f7ec8388808dd019ead9c692736c74d81bfab
Peter Backman <peter@iostream.cc>
2019-11-27 22:35:06 +0100

    Flip buffers can be used for allocating data without freeing. Similar to a ring buffer but it's very clear when an allocation has been overwritten/cleared

---
+++ support/flip_buffer.h
@@ -0,0 +1,100 @@
+// -*- c++ -*-
+
+#ifndef PEOS_SUPPORT_FLIP_BUFFER_H
+#define PEOS_SUPPORT_FLIP_BUFFER_H
+
+#include <stddef.h>
+
+namespace p2 {
+  template<size_t _MaxLen>
+  class flip_buffer {
+  public:
+    typedef uint64_t handle;
+
+    flip_buffer()
+    {
+      _buffers[0].generation = 1000;
+      _buffers[1].generation = 60000;
+    }
+
+    handle alloc(size_t length)
+    {
+      if (length > _MaxLen)
+        return 0;
+
+      if (length > _MaxLen - _cur_buf->size) {
+        // No space, change current allocating buffer and invalidate
+        // all existing handles
+        flip();
+        _cur_buf->reset();
+      }
+
+      handle new_ptr = create_ptr(_cur_buf == &_buffers[0] ? 1 : 2, _cur_buf->generation, _cur_buf->size);
+      _cur_buf->size += length;
+      return new_ptr;
+    }
+
+    char *data(handle handle_)
+    {
+      buffer *buf;
+
+      if (ptr_buffer(handle_) == 1)
+        buf = &_buffers[0];
+      else if (ptr_buffer(handle_) == 2)
+        buf = &_buffers[1];
+      else
+        return nullptr;
+
+
+      if (ptr_generation(handle_) != buf->generation)
+        return nullptr;
+
+      if (ptr_offset(handle_) >= buf->size)
+        return nullptr;
+
+      return buf->data + ptr_offset(handle_);
+    }
+
+  private:
+    static inline uint8_t ptr_buffer(handle ptr)
+    {
+      return ptr >> 62;
+    }
+
+    static inline uint16_t ptr_generation(handle ptr)
+    {
+      return (ptr & 0x0000FFFF00000000) >> 32;
+    }
+
+    static inline uint32_t ptr_offset(handle ptr)
+    {
+      return ptr & 0x00000000FFFFFFFF;
+    }
+
+    static inline handle create_ptr(uint8_t buffer, uint16_t gen, uint32_t offset)
+    {
+      return ((uint64_t)buffer << 62) | ((uint64_t)gen << 32) | (uint64_t)offset;
+    }
+
+    void flip()
+    {
+      if (_cur_buf == &_buffers[0])
+        _cur_buf = &_buffers[1];
+      else
+        _cur_buf = &_buffers[0];
+    }
+
+    struct buffer {
+      void reset() { size = 0; ++generation; }
+
+      size_t size = 0;
+      char data[_MaxLen];
+      uint16_t generation = 0;
+    };
+
+    buffer _buffers[2];
+    buffer *_cur_buf = &_buffers[0];
+  };
+}
+
+#endif // !PEOS_SUPPORT_FLIP_BUFFER_H
---
+++ support/unittests/test_flip_buffer.cc
@@ -0,0 +1,44 @@
+#include "unittest.h"
+#include "support/flip_buffer.h"
+
+TESTSUITE(p2::flip_buffer) {
+  TESTCASE("allocating more than _MaxLen returns an invalid handle") {
+    p2::flip_buffer<1024> buf;
+    auto handle = buf.alloc(1025);
+    ASSERT_EQ(buf.data(handle), nullptr);
+  }
+
+  TESTCASE("can allocate the full _MaxLen") {
+    p2::flip_buffer<1024> buf;
+    auto handle = buf.alloc(1024);
+    ASSERT_NEQ(buf.data(handle), nullptr);
+  }
+
+  TESTCASE("flipping between buffers invalidates pointers") {
+    p2::flip_buffer<1024> buf;
+
+    auto handle1 = buf.alloc(512);
+    auto handle2 = buf.alloc(1024);
+
+    ASSERT_NEQ(buf.data(handle1), nullptr);
+    ASSERT_NEQ(buf.data(handle2), nullptr);
+
+    auto handle3 = buf.alloc(1);
+    ASSERT_EQ(buf.data(handle1), nullptr);
+    ASSERT_NEQ(buf.data(handle2), nullptr);
+    ASSERT_NEQ(buf.data(handle3), nullptr);
+
+    auto handle4 = buf.alloc(1023);
+    ASSERT_EQ(buf.data(handle1), nullptr);
+    ASSERT_NEQ(buf.data(handle2), nullptr);
+    ASSERT_NEQ(buf.data(handle3), nullptr);
+    ASSERT_NEQ(buf.data(handle4), nullptr);
+
+    auto handle5 = buf.alloc(256);
+    ASSERT_EQ(buf.data(handle1), nullptr);
+    ASSERT_EQ(buf.data(handle2), nullptr);
+    ASSERT_NEQ(buf.data(handle3), nullptr);
+    ASSERT_NEQ(buf.data(handle4), nullptr);
+    ASSERT_NEQ(buf.data(handle5), nullptr);
+  }
+}
