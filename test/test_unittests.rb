a353ea6fb6c02f42ffdb3db23ebae86f40afe6cb
Peter Backman <peter@iostream.cc>
2019-12-01 13:38:17 +0100

    Run program unittests in the complete test suite

--- test/test_unittests.rb
+++ test/test_unittests.rb
@@ -10,3 +10,16 @@ scenario "libsupport unittest" do
     successfully_executes
   end
 end
+
+scenario "programs unittest" do
+  builds [
+    %q(OPT_FLAGS="-O0" make -C programs -f Makefile.host clean all),
+    %q(OPT_FLAGS="-O3" make -C programs -f Makefile.host clean all),
+  ]
+
+  command "programs/unittest"
+
+  it "runs successfully" do
+    successfully_executes
+  end
+end
