a4a786943f2dfec8d8cf0ecacb892935597c1327
Peter Backman <peter@iostream.cc>
2019-12-01 12:40:42 +0100

    Support: build unittests and library in .host directory

--- test/test_unittests.rb
+++ test/test_unittests.rb
@@ -1,10 +1,10 @@
 scenario "libsupport unittest" do
   builds [
-    %q(OPT_FLAGS="-O0" make -C support -f Makefile.host clean unittest),
-    %q(OPT_FLAGS="-O3" make -C support -f Makefile.host clean unittest),
+    %q(OPT_FLAGS="-O0" make -C support -f Makefile.host clean all),
+    %q(OPT_FLAGS="-O3" make -C support -f Makefile.host clean all),
   ]
 
-  command "support/unittest"
+  command "support/.host/unittest"
 
   it "runs successfully" do
     successfully_executes
