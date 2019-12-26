bb7ffa750c0edad906dc598cd08d35c7c350cda9
Peter Backman <peter@iostream.cc>
2019-12-26 15:56:03 +0100

    Deduplicated makefiles in /support

--- test/test_unittests.rb
+++ test/test_unittests.rb
@@ -1,7 +1,7 @@
 scenario "libsupport unittest" do
   builds [
-    %q(OPT_FLAGS="-O0" make -C support -f Makefile.host clean all),
-    %q(OPT_FLAGS="-O3" make -C support -f Makefile.host clean all),
+    %q(OPT_FLAGS="-O0" make -C support -f Makefile.host clean unittest),
+    %q(OPT_FLAGS="-O3" make -C support -f Makefile.host clean unittest),
   ]
 
   command "support/.host/unittest"
@@ -13,8 +13,8 @@ end
 
 scenario "programs unittest" do
   builds [
-    %q(OPT_FLAGS="-O0" make -C programs -f Makefile.host clean all),
-    %q(OPT_FLAGS="-O3" make -C programs -f Makefile.host clean all),
+    %q(OPT_FLAGS="-O0" make -C programs -f Makefile.host clean unittest),
+    %q(OPT_FLAGS="-O3" make -C programs -f Makefile.host clean unittest),
   ]
 
   command "programs/unittest"
