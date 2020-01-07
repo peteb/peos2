6e51cbf7b17678bd82eb6d34de085befd0ae5786
Peter Backman <peter@iostream.cc>
2020-01-07 20:02:26 +0100

    Fix broken unittests

--- programs/unittests/test_tcp_connection_table.cc
+++ programs/unittests/test_tcp_connection_table.cc
@@ -5,7 +5,7 @@ TESTSUITE(tcp_connection_table) {
   TESTCASE("find_best_match: finds a wildcard connection") {
     // given
     tcp_connection_table tab;
-    auto conn = tab.create_connection({0, 0}, {0, 0}, nullptr);
+    auto conn = tab.create_connection({0, 0}, {0, 0}, nullptr, {});
 
     // when
     auto match = tab.find_best_match({0x11223344, 4124}, {0x10101010, 8888});
@@ -17,12 +17,12 @@ TESTSUITE(tcp_connection_table) {
   TESTCASE("find_best_match: finds the most specific connection") {
     // given
     tcp_connection_table tab;
-    tab.create_connection({0, 0}, {0, 0}, nullptr);
-    tab.create_connection({0x44334455, 0}, {0, 0}, nullptr);
-    tab.create_connection({0x44334455, 2412}, {0, 0}, nullptr);
-    tab.create_connection({0x44334455, 2412}, {0x10101010, 0}, nullptr);
+    tab.create_connection({0, 0}, {0, 0}, nullptr, {});
+    tab.create_connection({0x44334455, 0}, {0, 0}, nullptr, {});
+    tab.create_connection({0x44334455, 2412}, {0, 0}, nullptr, {});
+    tab.create_connection({0x44334455, 2412}, {0x10101010, 0}, nullptr, {});
 
-    auto conn = tab.create_connection({0x44334455, 2412}, {0x10101010, 8888}, nullptr);
+    auto conn = tab.create_connection({0x44334455, 2412}, {0x10101010, 8888}, nullptr, {});
 
     // when
     auto match = tab.find_best_match({0x44334455, 2412}, {0x10101010, 8888});
@@ -34,7 +34,7 @@ TESTSUITE(tcp_connection_table) {
   TESTCASE("find_best_match: finds nothing when there is no match") {
     // given
     tcp_connection_table tab;
-    tab.create_connection({0, 0}, {0, 8080}, nullptr);
+    tab.create_connection({0, 0}, {0, 8080}, nullptr, {});
 
     // when
     auto match = tab.find_best_match({0x11223344, 4124}, {0x10101010, 8888});
